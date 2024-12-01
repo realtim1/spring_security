259c798fd6538c8edaa1a64fbe9b4e9ea032ec3f
realtim1 <timciki@naver.com>
2024-12-01 18:56:35 +0900

    security config

--- src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -1,34 +1,62 @@
 package io.security.springsecuritymaster.security.configs;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
 //                .authorizeHttpRequests(auth -> auth
 //                        .requestMatchers("/").permitAll()
 //                        .anyRequest().authenticated())
 //                .formLogin(Customizer.withDefaults())
-//        ;
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
-//        return  new InMemoryUserDetailsManager(user);
-//    }
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/loginPage")
+                        .loginProcessingUrl("/loginProc")
+                        .defaultSuccessUrl("/",true)
+                        .failureUrl("/failed")
+                        .usernameParameter("userId")
+                        .passwordParameter("passwd")
+                        .successHandler(new AuthenticationSuccessHandler(){
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                System.out.println("authentication : " + authentication);
+                                response.sendRedirect("/home");
+                            }
+                        })
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("exception : " + exception.getMessage());
+                            response.sendRedirect("/login");
+                        })
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}12345").roles("USER").build();
+        return  new InMemoryUserDetailsManager(user);
+    }
 }
